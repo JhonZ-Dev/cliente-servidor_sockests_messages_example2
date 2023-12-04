36b4e4e1e96205c70480cfbf95ae106b223434b3
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-12-03 21:23:54 -0500

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -6,3 +6,6 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
 server.listen(5)
 print(f"Servidor escuchando en {host}:{port}")
+# Esperar a que un cliente se conecte
+client_socket, addr = server.accept()
+print(f"Cliente conectado desde {addr[0]}:{addr[1]}")
