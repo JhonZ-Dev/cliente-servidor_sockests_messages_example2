a332edf333811f5d0e42fc7dc9553bba37f2ab05
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-12-03 21:21:37 -0500

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -6,3 +6,6 @@ port = 12345
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((host, port))
+# Enviar un mensaje al servidor
+message = "Hola, servidor"
+client.send(message.encode('utf-8'))
